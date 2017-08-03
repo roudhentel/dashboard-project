a025861d7ef0784175d69cfc68da59e64b2b30de
roudhentel <roudhentel.i.nagoy@gmail.com>
2017-08-03 14:13:05 +0800

    change client id for authentication

--- public/scripts/app/main-app.js
+++ public/scripts/app/main-app.js
@@ -1,48 +1,19 @@
-var mainApp = angular.module("mainApp", ["ui.router", "ngRoute", "ngMaterial", "gridster", "AdalAngular"]);
+var mainApp = angular.module("mainApp", ["ngRoute", "ngMaterial", "gridster", "AdalAngular"]);
 
-mainApp.config(($stateProvider, $urlRouterProvider, $locationProvider, adalAuthenticationServiceProvider, $httpProvider, $routeProvider) => {
-    $urlRouterProvider.otherwise('/dashboard');
-
-    $stateProvider
-        .state('login', {
-            url: "/login",
-            templateUrl: "contents/login/",
-            controller: "loginCtrl"
-        })
-        .state('login.authenticate', {
-            url: "/authenticate",
-            templateUrl: "contents/login/login-form.html",
-            controller: "loginFormCtrl"
-        })
-        .state('login.signup', {
-            url: "/signup",
-            templateUrl: "contents/login/signup.html",
-            controller: "signupCtrl"
-        });
-        // .state('home', {
-        //     url: "/home",
-        //     templateUrl: "contents/home/",
-        //     controller: "homeCtrl",
-        //     requireADLogin: true
-        // })
-        // .state('home.dashboard', {
-        //     url: "/home/dashboard",
-        //     templateUrl: "contents/dashboard/",
-        //     controller: "dashboardCtrl",
-        //     requireADLogin: true
-        // });
+mainApp.config(($locationProvider, adalAuthenticationServiceProvider, $httpProvider, $routeProvider) => {
 
     $routeProvider.when("/dashboard", {
         templateUrl: "contents/dashboard/",
         controller: "dashboardCtrl",
         requireADLogin: true
-    });
+    }).otherwise({ redirectTo: "/dashboard" });
 
     // initialize ADAL with your application's information
     adalAuthenticationServiceProvider.init({
         instance: 'https://login.microsoftonline.com/',
         tenant: 'common',
         clientId: '238ebf3d-2ed8-4e49-ac37-0ac95b6a28a5',
+        // clientId: 'ce203717-c038-45c0-ad87-3c7bd40869d3',
     }, $httpProvider);
 
     $locationProvider.hashPrefix('');
